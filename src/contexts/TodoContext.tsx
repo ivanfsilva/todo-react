f4c00f291f7ed379622ebf1bff8182137fc3a6dc
ivanfsilva <ivanfs27@gmail.com>
2020-10-07 14:40:27 -0300

    React Context

---
+++ src/contexts/TodoContext.tsx
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react';
+
+import { TodoContextType } from './TodoContextType';
+
+export const TodoContext = createContext<TodoContextType>({
+    todos: [],
+    addTodo: () => { },
+    removeTodo: () => { },
+    toggle: () => { },
+});
