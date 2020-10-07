6de136f1ce36ed389ac83d2fb733a6937190e208
ivanfsilva <ivanfs27@gmail.com>
2020-10-07 12:59:54 -0300

    criando o item da lista

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { Todo } from '../models/Todo';
+import TodoListItem from './TodoListIteim';
 
 const TodoList = () => { 
     const todos: Todo[] = [
         {id: 1, title: 'Ir ao supermercado', done: false},
         {id: 2, title: 'Ir a academia', done: false}
     ];
-    return(<div>Todo List</div>);
+    return(
+        <table className="uk-table">
+            <caption>Lista de Tarefas</caption>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Tarefa</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    todos?.map(
+                    todo => (<TodoListItem key={todo.id}></TodoListItem>)
+                    )
+                }
+            </tbody>
+        </table>
+    );
 }
 
 export default TodoList;
