49d50dcbe8b85f2cca9446d154b7329c2c424c5c
ivanfsilva <ivanfs27@gmail.com>
2020-10-07 10:19:30 -0300

    criando menu de navegacao

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Navbar from './Navbar';
 
-const App = () => { }
+const App = () => { 
+    return(
+        <div>
+            <Navbar></Navbar>
+            <h1>Meu Primeiro App</h1> 
+        </div>);
+}
 
 export default App;
