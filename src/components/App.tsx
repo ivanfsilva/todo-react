d93ad2df6178e359e8626834dc7ec12a596a8265
ivanfsilva <ivanfs27@gmail.com>
2020-10-07 15:10:28 -0300

    utilizando o context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+
+import TodoContext from '../contexts/TodoContext';
 import Navbar from './Navbar';
 import TodoList from './TodoList';
 
 const App = () => { 
     return(
-        <div className="uk-container">
-            <Navbar></Navbar>
-            <TodoList></TodoList>           
-        </div>);
+        <TodoContext>
+            <div className="uk-container">
+                <Navbar></Navbar>
+                <TodoList></TodoList>           
+            </div>      
+        </TodoContext>
+    );
 }
 
 export default App;
--- src/components/TodoListIteim.tsx
+++ src/components/TodoListIteim.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import TodoProvider, { TodoContext } from '../contexts/TodoContext';
+import { TodoContextType } from '../contexts/TodoContextType';
 import { Todo } from '../models/Todo';
 
 interface TodoListItemProps {
@@ -7,12 +9,14 @@ interface TodoListItemProps {
 
 const TodoListItem = (props: TodoListItemProps) => {
 
+    const { removeTodo, toggle } = useContext<TodoContextType>(TodoContext);
+
     const onRemove = (todo: Todo) => {
-        console.log(todo);
+        removeTodo(todo);
     }
 
     const handleChange = (event: any) => { 
-        console.log('Mudou');
+        toggle(props.todo);
     }
 
     return(
